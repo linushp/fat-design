89a0509daf5ff1813771a497587323859777c438
hellomaokai2025 <hellomaokai2025@foxmail.com>
2026-09-06 20:25:10 +0800

    React版本兼容16/17/18

--- libs/config-19.js
+++
@@ -1,9 +0,0 @@
-import * as ReactDOMClient from 'react-dom/client'
-import * as ReactDOM from 'react-dom'
-import { configReactDOM18 , logger } from "fat-design";
-import 'fat-design/style.css'
-function prepareLoadAssets() {
-    configReactDOM18(ReactDOM, ReactDOMClient);
-    logger.setLogEnable(false);
-}
-prepareLoadAssets();
